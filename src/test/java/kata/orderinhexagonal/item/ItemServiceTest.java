dce03566ecf018eb17afae5db782e36be72c5a5a
ejoongseok <ejoongseok@gmail.com>
2022-06-29 16:23:06 +0900

    상품 등록 테스트케이스 작성

---
+++ src/test/java/kata/orderinhexagonal/item/ItemServiceTest.java
@@ -0,0 +1,16 @@
+package kata.orderinhexagonal.item;
+
+import org.junit.jupiter.api.Test;
+
+class ItemServiceTest {
+
+	@Test
+	void createItemTest() {
+		// given
+
+		// when
+
+		// then
+	}
+
+}
