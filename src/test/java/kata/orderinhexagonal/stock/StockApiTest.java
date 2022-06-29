ae16a2d95cd59200904c106dc98f60c1984e7832
ejoongseok <ejoongseok@gmail.com>
2022-06-29 17:25:01 +0900

    상품 입고 Assertions 작성

--- src/test/java/kata/orderinhexagonal/stock/StockApiTest.java
+++ src/test/java/kata/orderinhexagonal/stock/StockApiTest.java
@@ -1,9 +1,15 @@
 package kata.orderinhexagonal.stock;
 
+import org.assertj.core.api.Assertions;
+import org.springframework.http.HttpStatus;
+
 class StockApiTest {
 
 	void 상품_입고() throws Exception {
-
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+		Assertions.assertThat(warehousingResponse.getId()).isPositive();
+		Assertions.assertThat(warehousingResponse.getQuantity()).isEqaulTo(quantity);
+		Assertions.assertThat(item.getStockQuantity()).isEqaulTo(quantity);
 	}
 
 	void 상품_출고() throws Exception {
