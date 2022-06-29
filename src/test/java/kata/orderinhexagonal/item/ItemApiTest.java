a3dcf55732e88479717612c12686cf0e8805a863
ejoongseok <ejoongseok@gmail.com>
2022-06-29 16:05:08 +0900

    added AutoConfigureMockMvc annotation

--- src/test/java/kata/orderinhexagonal/item/ItemApiTest.java
+++ src/test/java/kata/orderinhexagonal/item/ItemApiTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,6 +18,7 @@ import kata.orderinhexagonal.item.application.port.in.CreateItemRequest;
 import kata.orderinhexagonal.item.application.port.in.CreateItemResponse;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ItemApiTest {
 
 	@Autowired MockMvc mockMvc;
