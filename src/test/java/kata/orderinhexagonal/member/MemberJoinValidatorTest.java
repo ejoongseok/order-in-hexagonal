7d2c6fc993e1511457d288db405a5060ade413b8
ejoongseok <ejoongseok@gmail.com>
2022-06-29 10:43:28 +0900

    MemberJoinValidator exists email 테스트 케이스 추가

--- src/test/java/kata/orderinhexagonal/member/MemberJoinValidatorTest.java
+++ src/test/java/kata/orderinhexagonal/member/MemberJoinValidatorTest.java
@@ -1,11 +1,22 @@
 package kata.orderinhexagonal.member;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import kata.orderinhexagonal.member.adapter.out.persistence.CreateMemberAdapter;
+import kata.orderinhexagonal.member.application.port.out.MemberJoinValidator;
 
 class MemberJoinValidatorTest {
 
+	private final MemberJoinValidator memberJoinValidator = new CreateMemberAdapter();
+
 	@Test
 	이메일_존재여부_확인() {
-
+		//given
+		String email = "ejoongseok@gmail.com";
+		//when
+		boolean verify = memberJoinValidator.existsEmail(email);
+		//then
+		Assertions.assertThat(verify).isFalse();
 	}
 }
