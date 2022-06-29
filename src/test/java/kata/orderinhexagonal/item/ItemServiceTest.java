29c1a30eacaed6ef1fd1ca0452ecd0b410d7fd8f
ejoongseok <ejoongseok@gmail.com>
2022-06-29 16:27:09 +0900

    상품 등록 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/item/ItemServiceTest.java
+++ src/test/java/kata/orderinhexagonal/item/ItemServiceTest.java
@@ -1,16 +1,41 @@
 package kata.orderinhexagonal.item;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import kata.orderinhexagonal.item.application.port.in.CreateItemRequest;
+import kata.orderinhexagonal.item.application.port.in.CreateItemUsecase;
+import kata.orderinhexagonal.item.domain.Item;
+
 class ItemServiceTest {
+
+	ItemService itemService;
 
 	@Test
 	void createItemTest() {
 		// given
+		String name = "노트북";
+		int price = 1_000_000;
+		CreateItemRequest request = CreateItemRequest.of(name, price);
 
 		// when
-
+		Item newItem = itemService.createItem(request);
 		// then
+		Assertions.assertThat(newItem.getId()).isPositive();
+		Assertions.assertThat(newItem.getName()).isEqualTo(name);
+		Assertions.assertThat(newItem.getPrice()).isEqualTo(price);
+		Assertions.assertThat(newItem.getStockQuantity()).isZero();
 	}
 
+	private static class ItemService implements CreateItemUsecase {
+
+		private SaveItemPort saveItemPort;
+
+		@Override
+		public Item createItem(CreateItemRequest request) {
+			Item item = new Item(request);
+			saveItemPort.saveItem(item);
+			return item;
+		}
+	}
 }
