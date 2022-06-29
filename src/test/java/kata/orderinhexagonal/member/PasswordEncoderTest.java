83ce5e26ad8ae7713af836115dd740a3b619e907
ejoongseok <ejoongseok@gmail.com>
2022-06-29 10:33:17 +0900

    passwordEncoder 구현체 작성

--- src/test/java/kata/orderinhexagonal/member/PasswordEncoderTest.java
+++ src/test/java/kata/orderinhexagonal/member/PasswordEncoderTest.java
@@ -1,9 +1,14 @@
 package kata.orderinhexagonal.member;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import kata.orderinhexagonal.member.application.port.out.PasswordEncoder;
+
 class PasswordEncoderTest {
+
+	private PasswordEncoder passwordEncoder = new CreateMemberAdapter();
 
 	@Test
 	void 비밀번호_인코딩() {
@@ -15,4 +20,10 @@ class PasswordEncoderTest {
 		Assertions.assertThat(password).isNotEqualTo(encodedPassword);
 	}
 
+	private static class CreateMemberAdapter implements PasswordEncoder {
+		@Override
+		public String encode(String password) {
+			return new DigestUtils("SHA3-256").digestAsHex(password);
+		}
+	}
 }
