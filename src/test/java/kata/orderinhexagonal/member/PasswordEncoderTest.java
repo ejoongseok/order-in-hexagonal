227ca750b972303e6f83d8c8d50a653834a213d1
ejoongseok <ejoongseok@gmail.com>
2022-06-29 10:16:10 +0900

    비밀번호 인코딩 테스트 케이스 추가

--- src/test/java/kata/orderinhexagonal/member/PasswordEncoderTest.java
+++ src/test/java/kata/orderinhexagonal/member/PasswordEncoderTest.java
@@ -1,4 +1,18 @@
 package kata.orderinhexagonal.member;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class PasswordEncoderTest {
+
+	@Test
+	void 비밀번호_인코딩() {
+		// given
+		String password = "ejoongseok1234!";
+		// when
+		String encodedPassword = passwordEncoder.encode(password);
+		// then
+		Assertions.assertThat(password).isNotEqualTo(encodedPassword);
+	}
+
 }
