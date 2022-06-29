f51404915ce2efb6b173e8137ae8940ad4701061
ejoongseok <ejoongseok@gmail.com>
2022-06-29 17:57:25 +0900

    added field validation annotation

--- src/main/java/kata/orderinhexagonal/stock/application/port/in/StockInRequest.java
+++ src/main/java/kata/orderinhexagonal/stock/application/port/in/StockInRequest.java
@@ -1,11 +1,22 @@
 package kata.orderinhexagonal.stock.application.port.in;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StockInRequest {
-	private final Long itemId;
-	private final int quantity;
+	@NotNull(message = "상품 ID를 입력해주세요.")
+	@Min(value = 1, message = "잘못된 상품 ID 입니다.")
+	private Long itemId;
+	@NotNull(message = "상품 수량을 입력해주세요.")
+	@Min(value = 1, message = "입고 수량을 1개 이상으로 입력해주세요.")
+	private Integer quantity;
 
 	public StockInRequest(Long itemId, int quantity) {
 
