fff53e5effa91dc9c3044de744d5d202e9db13f4
ejoongseok <ejoongseok@gmail.com>
2022-06-29 09:48:21 +0900

    added lombok getter, default constructor

--- src/main/java/kata/orderinhexagonal/member/application/port/in/CreateMemberResponse.java
+++ src/main/java/kata/orderinhexagonal/member/application/port/in/CreateMemberResponse.java
@@ -1,24 +1,14 @@
 package kata.orderinhexagonal.member.application.port.in;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateMemberResponse {
 	private long id;
 	private String name;
 	private String email;
 	private String location;
-
-	public long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getLocation() {
-		return location;
-	}
 }
