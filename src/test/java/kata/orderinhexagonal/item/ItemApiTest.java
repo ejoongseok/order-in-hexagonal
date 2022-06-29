791c93f9cb5b2e1a7ffa0073137aca66f08eebbb
ejoongseok <ejoongseok@gmail.com>
2022-06-29 15:41:38 +0900

    상품 등록 Assertions 작성

--- src/test/java/kata/orderinhexagonal/item/ItemApiTest.java
+++ src/test/java/kata/orderinhexagonal/item/ItemApiTest.java
@@ -1,4 +1,23 @@
 package kata.orderinhexagonal.item;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
 class ItemApiTest {
+
+	@Test
+	void 상품_등록() {
+		// given
+
+		// when
+
+		// then
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+		Assertions.assertThat(createItemResponse.getId()).isPositive();
+		Assertions.assertThat(createItemResponse.getName()).isEqualTo(name);
+		Assertions.assertThat(createItemResponse.getPrice()).isEqualTo(price);
+		Assertions.assertThat(createItemResponse.getStockQuantity()).isEqualTo(stockQuantity);
+	}
+
 }
