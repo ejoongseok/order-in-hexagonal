d0b1059a34b7231ecfaec6421bc0b2ebab3451c7
ejoongseok <ejoongseok@gmail.com>
2022-06-29 10:44:13 +0900

    implements MEmberJoinValidator

--- src/main/java/kata/orderinhexagonal/member/adapter/out/persistence/CreateMemberAdapter.java
+++ src/main/java/kata/orderinhexagonal/member/adapter/out/persistence/CreateMemberAdapter.java
@@ -2,11 +2,17 @@ package kata.orderinhexagonal.member.adapter.out.persistence;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import kata.orderinhexagonal.member.application.port.out.MemberJoinValidator;
 import kata.orderinhexagonal.member.application.port.out.PasswordEncoder;
 
-public class CreateMemberAdapter implements PasswordEncoder {
+public class CreateMemberAdapter implements PasswordEncoder, MemberJoinValidator {
 	@Override
 	public String encode(String password) {
 		return new DigestUtils("SHA3-256").digestAsHex(password);
+	}
+
+	@Override
+	public boolean existsEmail(String email) {
+		return false;
 	}
 }
