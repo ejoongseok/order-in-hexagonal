f8321ff2950cac3007e1350f6f58f84676aa38ac
ejoongseok <ejoongseok@gmail.com>
2022-06-29 10:05:05 +0900

    Member Domain import

--- src/main/java/kata/orderinhexagonal/member/application/port/in/CreateMemberUsecase.java
+++ src/main/java/kata/orderinhexagonal/member/application/port/in/CreateMemberUsecase.java
@@ -1,5 +1,7 @@
 package kata.orderinhexagonal.member.application.port.in;
 
+import kata.orderinhexagonal.member.domain.Member;
+
 public interface CreateMemberUsecase {
 
 	Member join(CreateMemberRequest request);
