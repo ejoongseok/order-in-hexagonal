ffea6901d56afb76e8e7e068232a24ff43d49969
ejoongseok <ejoongseok@gmail.com>
2022-06-29 15:59:36 +0900

    인바운드 어댑터 생성

---
+++ src/main/java/kata/orderinhexagonal/item/adapter/in/web/ItemController.java
@@ -0,0 +1,11 @@
+package kata.orderinhexagonal.item.adapter.in.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+
+}
