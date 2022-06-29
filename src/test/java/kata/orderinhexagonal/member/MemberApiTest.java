651aa1ee3c7889ac170f110e440c61298fd22fb2
ejoongseok <ejoongseok@gmail.com>
2022-06-29 09:32:17 +0900

    회원 가입 테스트 케이스 Assertions 작성

--- src/test/java/kata/orderinhexagonal/member/MemberApiTest.java
+++ src/test/java/kata/orderinhexagonal/member/MemberApiTest.java
@@ -1,8 +1,17 @@
 package kata.orderinhexagonal.member;
 
+import org.assertj.core.api.Assertions;
+import org.springframework.http.HttpStatus;
+
 class MemberApiTest {
 
 	void 회원가입() {
 
+		//then
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+		Assertions.assertThat(createMemberResponse.getId()).isPositive();
+		Assertions.assertThat(createMemberResponse.getName()).isEqualTo(name);
+		Assertions.assertThat(createMemberResponse.getEmail()).isEqualTo(email);
+		Assertions.assertThat(createMemberResponse.getLocation).isEqualTo(location);
 	}
 }
