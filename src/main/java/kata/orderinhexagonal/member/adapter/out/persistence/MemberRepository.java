1a12f42b3eb681ee4d3e8ade61b623a2a925ff11
ejoongseok <ejoongseok@gmail.com>
2022-06-29 14:18:16 +0900

    added save method

--- src/main/java/kata/orderinhexagonal/member/adapter/out/persistence/MemberRepository.java
+++ src/main/java/kata/orderinhexagonal/member/adapter/out/persistence/MemberRepository.java
@@ -1,5 +1,9 @@
 package kata.orderinhexagonal.member.adapter.out.persistence;
 
+import kata.orderinhexagonal.member.domain.Member;
+
 public interface MemberRepository {
 	boolean existsByEmail(String email);
+
+	void save(Member member);
 }
--- src/main/java/kata/orderinhexagonal/member/adapter/out/persistence/MemoryMemberRepository.java
+++ src/main/java/kata/orderinhexagonal/member/adapter/out/persistence/MemoryMemberRepository.java
@@ -2,15 +2,27 @@ package kata.orderinhexagonal.member.adapter.out.persistence;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 import kata.orderinhexagonal.member.domain.Member;
 
 public class MemoryMemberRepository	implements MemberRepository {
 
 	private final Map<Long, Member> memoryPersistence = new ConcurrentHashMap<>();
+	private final AtomicLong sequence = new AtomicLong(1);
 
 	@Override
 	public boolean existsByEmail(String email) {
 		return memoryPersistence.values().stream().anyMatch(member -> member.getEmail().equals(email));
+	}
+
+	@Override
+	public void save(Member member) {
+		member.assignId(nextId());
+		memoryPersistence.put(member.getId(), member);
+	}
+
+	private long nextId() {
+		return sequence.incrementAndGet();
 	}
 }
