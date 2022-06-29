cb0ebbc769f38124f66b51621796c28c842ebf76
ejoongseok <ejoongseok@gmail.com>
2022-06-29 10:49:09 +0900

    existsByEmail 로직 추가

--- src/main/java/kata/orderinhexagonal/member/adapter/out/persistence/MemoryMemberRepository.java
+++ src/main/java/kata/orderinhexagonal/member/adapter/out/persistence/MemoryMemberRepository.java
@@ -1,8 +1,16 @@
 package kata.orderinhexagonal.member.adapter.out.persistence;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import kata.orderinhexagonal.member.domain.Member;
+
 public class MemoryMemberRepository	implements MemberRepository {
+
+	private final Map<Long, Member> memoryPersistence = new ConcurrentHashMap<>();
+
 	@Override
 	public boolean existsByEmail(String email) {
-		return false;
+		return memoryPersistence.values().stream().anyMatch(member -> member.getEmail().equals(email));
 	}
 }
