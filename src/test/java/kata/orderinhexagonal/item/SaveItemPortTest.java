4080f84579db332794e9b59f2c4b38d1fe049f1b
ejoongseok <ejoongseok@gmail.com>
2022-06-29 16:34:29 +0900

    save item 테스트 케이스 작성

---
+++ src/test/java/kata/orderinhexagonal/item/SaveItemPortTest.java
@@ -0,0 +1,28 @@
+package kata.orderinhexagonal.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import kata.orderinhexagonal.item.domain.Item;
+
+class SaveItemPortTest {
+
+	private SaveItemPort saveItemPort;
+
+	@Test
+	void saveItemTest() {
+		// given
+		String name = "노트북";
+		int price = 1_000_000;
+		Item item = new Item(name, price);
+		// when
+	    saveItemPort.saveItem(item);
+		// then
+		Assertions.assertThat(item.getId()).isPositive();
+		Assertions.assertThat(item.getName()).isEqualTo(name);
+		Assertions.assertThat(item.getPrice()).isEqualTo(price);
+		Assertions.assertThat(item.getStockQuantity()).isZero();
+
+	}
+
+}
