65f26cc18c10ee81cc52a92ee944b769990283d8
ejoongseok <ejoongseok@gmail.com>
2022-06-30 16:42:54 +0900

    itemId로 item 조회 기능 추가

--- src/test/java/kata/orderinhexagonal/fixture/ItemFixture.java
+++ src/test/java/kata/orderinhexagonal/fixture/ItemFixture.java
@@ -3,6 +3,9 @@ package kata.orderinhexagonal.fixture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import kata.orderinhexagonal.item.adapter.out.persistence.ItemEntity;
+import kata.orderinhexagonal.item.adapter.out.persistence.ItemMapper;
+import kata.orderinhexagonal.item.adapter.out.persistence.ItemRepository;
 import kata.orderinhexagonal.item.application.port.in.CreateItemRequest;
 import kata.orderinhexagonal.item.application.service.ItemService;
 import kata.orderinhexagonal.item.domain.Item;
@@ -12,7 +15,19 @@ public class ItemFixture {
 
 	@Autowired ItemService itemService;
 
+	@Autowired
+	ItemRepository itemRepository;
+
+	@Autowired
+	ItemMapper itemMapper;
+
 	public Item createItem(String name, int price) {
 		return itemService.createItem(CreateItemRequest.of(name, price));
+	}
+
+	public Item getItem(Long id) {
+		ItemEntity itemEntity = itemRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("Item not found"));
+		return itemMapper.toDomain(itemEntity);
 	}
 }
