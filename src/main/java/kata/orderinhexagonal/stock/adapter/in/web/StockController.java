bc2f436ee8cf70cdb319784a4a3a0bda0337e09a
ejoongseok <ejoongseok@gmail.com>
2022-06-30 16:54:55 +0900

    상품 출고 API 추가

--- src/main/java/kata/orderinhexagonal/stock/adapter/in/web/StockController.java
+++ src/main/java/kata/orderinhexagonal/stock/adapter/in/web/StockController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import kata.orderinhexagonal.stock.application.port.in.StockInRequest;
 import kata.orderinhexagonal.stock.application.port.in.StockInResponse;
 import kata.orderinhexagonal.stock.application.port.in.StockInUsecase;
+import kata.orderinhexagonal.stock.application.port.in.StockOutRequest;
+import kata.orderinhexagonal.stock.application.port.in.StockOutResponse;
 import kata.orderinhexagonal.stock.domain.Stock;
 import lombok.RequiredArgsConstructor;
 
@@ -26,7 +28,12 @@ public class StockController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public StockInResponse stockIn(@RequestBody @Valid StockInRequest request) {
 		Stock stock = stockInUsecase.stockIn(request);
-
 		return new StockInResponse(stock.getId(), stock.getItem().getId(), stock.getQuantity(), stock.getItem().getName());
+	}
+
+	@PostMapping("/out")
+	@ResponseStatus(HttpStatus.OK)
+	public StockOutResponse stockOut(@RequestBody @Valid StockOutRequest request) {
+		return null;
 	}
 }
