3840e2fde09739649fcd1f6f63871102c08b2c27
ejoongseok <ejoongseok@gmail.com>
2022-06-30 15:33:18 +0900

    added All Argument Constructor

--- src/main/java/kata/orderinhexagonal/stock/application/port/in/StockInResponse.java
+++ src/main/java/kata/orderinhexagonal/stock/application/port/in/StockInResponse.java
@@ -1,5 +1,6 @@
 package kata.orderinhexagonal.stock.application.port.in;
 
+import kata.orderinhexagonal.stock.domain.Stock;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,10 @@ public class StockInResponse {
 	private int quantity;
 	private String itemName;
 
+	public StockInResponse(Long id, Long itemId, Integer quantity, String itemName) {
+		this.id = id;
+		this.itemId = itemId;
+		this.quantity = quantity;
+		this.itemName = itemName;
+	}
 }
