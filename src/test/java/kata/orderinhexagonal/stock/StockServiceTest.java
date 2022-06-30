ccf33961d2f356a627bc04ef25f8f961e7ddf0f7
ejoongseok <ejoongseok@gmail.com>
2022-06-30 17:25:08 +0900

    Spring boot test migration

--- src/test/java/kata/orderinhexagonal/stock/StockServiceTest.java
+++ src/test/java/kata/orderinhexagonal/stock/StockServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import kata.orderinhexagonal.fixture.ItemFixture;
+import kata.orderinhexagonal.fixture.StockFixture;
 import kata.orderinhexagonal.item.domain.Item;
 import kata.orderinhexagonal.stock.application.port.in.StockInRequest;
 import kata.orderinhexagonal.stock.application.service.StockService;
@@ -17,6 +18,9 @@ class StockServiceTest {
 	@Autowired ItemFixture itemFixture;
 	@Autowired
 	StockService stockService;
+
+	@Autowired
+	StockFixture stockFixture;
 
 	@Test
 	void stockInTest() {
@@ -34,5 +38,27 @@ class StockServiceTest {
 		Assertions.assertThat(stockInItem.getId()).isEqualTo(item.getId());
 		Assertions.assertThat(stockInItem.getName()).isEqualTo(item.getName());
 	}
+
+	@Test
+	void stockOutTest() {
+		//given
+		Item item = itemFixture.createItem("노트북", 1_000_000);
+		int stockOutQuantity = 5;
+		int stockInQuantity = 10;
+		stockFixture.stockIn(item, stockInQuantity);
+		Item stockInItem = itemFixture.getItem(item.getId());
+		int currentQuantity = stockInItem.getStockQuantity() - stockOutQuantity;
+
+		// when
+		Stock stockOut = stockService.stockOut(item.getId(), stockOutQuantity);
+		// then
+		Assertions.assertThat(stockOut.getId()).isPositive();
+		Assertions.assertThat(stockOut.getStockType()).isEqualTo(Stock.StockType.STOCK_OUT);
+		Assertions.assertThat(stockOut.getQuantity()).isEqualTo(stockOutQuantity);
+		Assertions.assertThat(stockOut.getItem().getStockQuantity()).isEqualTo(currentQuantity);
+		Assertions.assertThat(stockOut.getItem().getId()).isEqualTo(item.getId());
+		Assertions.assertThat(stockOut.getItem().getName()).isEqualTo(item.getName());
+	}
+
 
 }
