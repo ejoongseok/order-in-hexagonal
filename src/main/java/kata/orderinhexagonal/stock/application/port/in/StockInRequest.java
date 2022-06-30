c9cfa3324c8bb8f99ec36a2e6f1752f1b745ee4b
ejoongseok <ejoongseok@gmail.com>
2022-06-30 17:39:25 +0900

    rename method StockInRequest.getQuantity -> getStockInQuantity

--- src/main/java/kata/orderinhexagonal/stock/application/port/in/StockInRequest.java
+++ src/main/java/kata/orderinhexagonal/stock/application/port/in/StockInRequest.java
@@ -1,7 +1,6 @@
 package kata.orderinhexagonal.stock.application.port.in;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import lombok.AccessLevel;
@@ -16,12 +15,12 @@ public class StockInRequest {
 	private Long itemId;
 	@NotNull(message = "상품 수량을 입력해주세요.")
 	@Min(value = 1, message = "입고 수량을 1개 이상으로 입력해주세요.")
-	private Integer quantity;
+	private Integer stockInQuantity;
 
-	public StockInRequest(Long itemId, int quantity) {
+	public StockInRequest(Long itemId, int stockInQuantity) {
 
 		this.itemId = itemId;
-		this.quantity = quantity;
+		this.stockInQuantity = stockInQuantity;
 	}
 
 	public static StockInRequest of(Long itemId, int quantity) {
--- src/main/java/kata/orderinhexagonal/stock/application/service/StockService.java
+++ src/main/java/kata/orderinhexagonal/stock/application/service/StockService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import kata.orderinhexagonal.item.domain.Item;
 import kata.orderinhexagonal.stock.application.port.in.StockInRequest;
 import kata.orderinhexagonal.stock.application.port.in.StockInUsecase;
-import kata.orderinhexagonal.stock.application.port.in.StockOutUsecase;
 import kata.orderinhexagonal.stock.application.port.out.LoadItemPort;
 import kata.orderinhexagonal.stock.application.port.out.SaveStockPort;
 import kata.orderinhexagonal.stock.domain.Stock;
@@ -14,21 +13,17 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class StockService implements StockInUsecase, StockOutUsecase {
+public class StockService implements StockInUsecase{
 	private final LoadItemPort loadItemPort;
 	private final SaveStockPort saveStockPort;
 
 	@Override
 	public Stock stockIn(StockInRequest request) {
 		Item item = loadItemPort.load(request.getItemId());
-		item.stockIn(request.getQuantity());
-		Stock stock = new StockIn(request.getQuantity(), item);
+		Integer stockInQuantity = request.getStockInQuantity();
+		item.stockInQuantity(stockInQuantity);
+		Stock stock = new StockIn(stockInQuantity, item);
 		saveStockPort.save(stock);
 		return stock;
-	}
-
-	@Override
-	public Stock stockOut(Long id, int stockOutQuantity) {
-		return null;
 	}
 }
