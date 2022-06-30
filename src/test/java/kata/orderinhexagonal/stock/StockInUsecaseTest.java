23e441dcd3f79da838b039f85c88a7347844b86c
ejoongseok <ejoongseok@gmail.com>
2022-06-30 09:31:01 +0900

    상품 입고 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/stock/StockInUsecaseTest.java
+++ src/test/java/kata/orderinhexagonal/stock/StockInUsecaseTest.java
@@ -3,20 +3,48 @@ package kata.orderinhexagonal.stock;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import kata.orderinhexagonal.fixture.ItemFixture;
+import kata.orderinhexagonal.item.domain.Item;
+import kata.orderinhexagonal.stock.application.port.in.StockInRequest;
+
 class StockInUsecaseTest {
+
+	ItemFixture itemFixture;
+	StockInUsecase stockInUsecase;
 
 	@Test
 	void 상품_입고() {
 		// given
+		Item item = itemFixture.createItem("노트북", 1_000_000);
+		int quantity = 1;
+		StockInRequest request = StockInRequest.of(item.getId(), quantity);
 
 		// when
-
+		Stock stock = stockInUsecase.stockIn(request);
 		// then
 		Assertions.assertThat(stock.getId()).isPositive();
 		Assertions.assertThat(stock.getQuantity()).isEqualTo(quantity);
+		Item stockInItem = stock.getItem();
 		Assertions.assertThat(stockInItem.getStockQuantity()).isEqualTo(quantity);
-		Assertions.assertThat(stockInItem.getId()).isEqualTo(itemId);
-		Assertions.assertThat(stockInItem.getName()).isEqualTo(itemName);
+		Assertions.assertThat(stockInItem.getId()).isEqualTo(item.getId());
+		Assertions.assertThat(stockInItem.getName()).isEqualTo(item.getName());
 	}
 
+	private static class Stock {
+		private Long id;
+		private Integer quantity;
+		private Item item;
+
+		public Long getId() {
+			return id;
+		}
+
+		public Integer getQuantity() {
+			return quantity;
+		}
+
+		public Item getItem() {
+			return item;
+		}
+	}
 }
