67448dc31d60bffea4f959b4741cc7b4180caf0e
ejoongseok <ejoongseok@gmail.com>
2022-06-30 10:14:33 +0900

    재고 저장 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/stock/SaveStockPortTest.java
+++ src/test/java/kata/orderinhexagonal/stock/SaveStockPortTest.java
@@ -1,4 +1,30 @@
 package kata.orderinhexagonal.stock;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import kata.orderinhexagonal.fixture.ItemFixture;
+import kata.orderinhexagonal.item.domain.Item;
+import kata.orderinhexagonal.stock.domain.Stock;
+import kata.orderinhexagonal.stock.domain.StockIn;
+
 class SaveStockPortTest {
+
+	ItemFixture itemFixture;
+	SaveStockPort saveStockPort;
+
+	@Test
+	void saveStockTest() {
+		// given
+		Item item = itemFixture.createItem("노트북", 1_000_000);
+		int quantity = 1;
+		Stock stock = new StockIn(quantity, item);
+		// when
+	    saveStockPort.save(stock);
+		// then
+		Assertions.assertThat(stock.getId()).isPositive();
+		Assertions.assertThat(stock.getQuantity()).isEqualTo(quantity);
+		Assertions.assertThat(stock.getStockType()).isEqualTo(Stock.StockType.STOCK_IN);
+	}
+
 }
