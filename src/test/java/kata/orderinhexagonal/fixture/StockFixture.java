74828e76a91ac92509016fd8f8828f57d5c9e36d
ejoongseok <ejoongseok@gmail.com>
2022-06-30 16:43:10 +0900

    스프링 빈으로 마이그레이션

--- src/test/java/kata/orderinhexagonal/fixture/StockFixture.java
+++ src/test/java/kata/orderinhexagonal/fixture/StockFixture.java
@@ -1,12 +1,16 @@
 package kata.orderinhexagonal.fixture;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import kata.orderinhexagonal.item.domain.Item;
 import kata.orderinhexagonal.stock.application.port.in.StockInRequest;
 import kata.orderinhexagonal.stock.application.port.in.StockInUsecase;
 
+@Component
 public class StockFixture {
 
-	StockInUsecase stockInUsecase;
+	@Autowired StockInUsecase stockInUsecase;
 
 	public void stockIn(Item item, int stockInQuantity) {
 		stockInUsecase.stockIn(StockInRequest.of(item.getId(), stockInQuantity));
