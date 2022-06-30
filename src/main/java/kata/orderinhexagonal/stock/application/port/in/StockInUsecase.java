6c0fa385df37ea0906b5472b5620011485b36410
ejoongseok <ejoongseok@gmail.com>
2022-06-30 09:38:00 +0900

    import stock domain

--- src/main/java/kata/orderinhexagonal/stock/application/port/in/StockInUsecase.java
+++ src/main/java/kata/orderinhexagonal/stock/application/port/in/StockInUsecase.java
@@ -1,5 +1,7 @@
 package kata.orderinhexagonal.stock.application.port.in;
 
+import kata.orderinhexagonal.stock.domain.Stock;
+
 public interface StockInUsecase {
 	Stock stockIn(StockInRequest request);
 }
