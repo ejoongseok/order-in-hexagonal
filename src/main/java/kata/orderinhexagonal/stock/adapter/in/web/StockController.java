77ab8503f515a35bb838f955fa2e82f9e78dca3f
ejoongseok <ejoongseok@gmail.com>
2022-06-30 15:32:55 +0900

    상품 입고 로직 추가

--- src/main/java/kata/orderinhexagonal/stock/adapter/in/web/StockController.java
+++ src/main/java/kata/orderinhexagonal/stock/adapter/in/web/StockController.java
@@ -11,14 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kata.orderinhexagonal.stock.application.port.in.StockInRequest;
 import kata.orderinhexagonal.stock.application.port.in.StockInResponse;
+import kata.orderinhexagonal.stock.application.port.in.StockInUsecase;
+import kata.orderinhexagonal.stock.domain.Stock;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/stock")
+@RequiredArgsConstructor
 public class StockController {
+
+	private final StockInUsecase stockInUsecase;
 
 	@PostMapping("/in")
 	@ResponseStatus(HttpStatus.CREATED)
 	public StockInResponse stockIn(@RequestBody @Valid StockInRequest request) {
-		return null;
+		Stock stock = stockInUsecase.stockIn(request);
+
+		return new StockInResponse(stock.getId(), stock.getItem().getId(), stock.getQuantity(), stock.getItem().getName());
 	}
 }
