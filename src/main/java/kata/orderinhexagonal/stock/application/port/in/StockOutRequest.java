a8061245f134f2ecb85ea04c4af7fe87e24e1149
ejoongseok <ejoongseok@gmail.com>
2022-06-30 16:55:21 +0900

    상품 출고 요청 필드 추가

--- src/main/java/kata/orderinhexagonal/stock/application/port/in/StockOutRequest.java
+++ src/main/java/kata/orderinhexagonal/stock/application/port/in/StockOutRequest.java
@@ -1,8 +1,28 @@
 package kata.orderinhexagonal.stock.application.port.in;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StockOutRequest {
 
-	public static StockOutRequest of(Long id, int stockOutQuantity) {
-		return null;
+	@NotNull(message = "상품 ID를 입력해주세요.")
+	private Long itemId;
+	@NotNull(message = "상품 출고 수량을 입력해주세요.")
+	@Min(value = 1, message = "출고 수량을 1개 이상으로 입력해주세요.")
+	private Integer stockOutQuantity;
+
+	public StockOutRequest(Long itemId, int stockOutQuantity) {
+		this.itemId = itemId;
+		this.stockOutQuantity = stockOutQuantity;
+	}
+
+	public static StockOutRequest of(Long itemId, int stockOutQuantity) {
+		return new StockOutRequest(itemId, stockOutQuantity);
 	}
 }
