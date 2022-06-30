7b91e782d177d127d3312f79462f0b20b937ddc3
ejoongseok <ejoongseok@gmail.com>
2022-06-30 09:39:41 +0900

    재고 입고 테스트 케이스 생성

--- src/test/java/kata/orderinhexagonal/stock/StockServiceTest.java
+++ src/test/java/kata/orderinhexagonal/stock/StockServiceTest.java
@@ -1,4 +1,17 @@
 package kata.orderinhexagonal.stock;
 
+import org.junit.jupiter.api.Test;
+
 class StockServiceTest {
+
+
+	@Test
+	void stockInTest() {
+		// given
+
+		// when
+
+		// then
+	}
+
 }
