6b20345a3af4e87f9565802e01f0d1e4a51182fc
ejoongseok <ejoongseok@gmail.com>
2022-06-30 09:17:09 +0900

    상품 입고 테스트 케이스 Assertions 추가

--- src/test/java/kata/orderinhexagonal/stock/StockInUsecaseTest.java
+++ src/test/java/kata/orderinhexagonal/stock/StockInUsecaseTest.java
@@ -1,5 +1,6 @@
 package kata.orderinhexagonal.stock;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class StockInUsecaseTest {
@@ -11,6 +12,11 @@ class StockInUsecaseTest {
 		// when
 
 		// then
+		Assertions.assertThat(stock.getId()).isPositive();
+		Assertions.assertThat(stock.getQuantity()).isEqualTo(quantity);
+		Assertions.assertThat(stockInItem.getStockQuantity()).isEqualTo(quantity);
+		Assertions.assertThat(stockInItem.getId()).isEqualTo(itemId);
+		Assertions.assertThat(stockInItem.getName()).isEqualTo(itemName);
 	}
 
 }
