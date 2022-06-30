d50e5015602ba307282d6afc6165f12e1e963a79
ejoongseok <ejoongseok@gmail.com>
2022-06-30 18:00:16 +0900

    added lombok

--- src/main/java/kata/orderinhexagonal/stock/application/port/in/StockOutResponse.java
+++ src/main/java/kata/orderinhexagonal/stock/application/port/in/StockOutResponse.java
@@ -1,24 +1,21 @@
 package kata.orderinhexagonal.stock.application.port.in;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StockOutResponse {
 	private long id;
 	private int quantity;
 	private long itemId;
 	private String itemName;
 
-	public long getId() {
-		return id;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public long getItemId() {
-		return itemId;
-	}
-
-	public String getItemName() {
-		return itemName;
+	public StockOutResponse(Long id, Integer quantity, Long itemId, String itemName) {
+		this.id = id;
+		this.quantity = quantity;
+		this.itemId = itemId;
+		this.itemName = itemName;
 	}
 }
