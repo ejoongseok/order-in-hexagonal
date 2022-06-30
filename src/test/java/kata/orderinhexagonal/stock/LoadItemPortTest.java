fa9e8b03fff8be3c7f3cddb7f0efd65512794251
ejoongseok <ejoongseok@gmail.com>
2022-06-30 10:24:32 +0900

    아이템 불러오기 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/stock/LoadItemPortTest.java
+++ src/test/java/kata/orderinhexagonal/stock/LoadItemPortTest.java
@@ -1,4 +1,27 @@
 package kata.orderinhexagonal.stock;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import kata.orderinhexagonal.fixture.ItemFixture;
+import kata.orderinhexagonal.item.domain.Item;
+
 class LoadItemPortTest {
+
+	ItemFixture itemFixture;
+	LoadItemPort loadItemPort;
+
+	@Test
+	void loadItemTest() {
+		// given
+		Item item = itemFixture.createItem("노트북", 1_000_000);
+		// when
+		Item loadItem = loadItemPort.load(item.getId());
+		// then
+		Assertions.assertThat(loadItem.getId()).isEqaulTo(item.getId());
+		Assertions.assertThat(loadItem.getName()).isEqaulTo(item.getName());
+		Assertions.assertThat(loadItem.getPrice()).isEqaulTo(item.getPrice());
+		Assertions.assertThat(loadItem.getStockQuantity()).isEqaulTo(item.getStockQuantity());
+	}
+
 }
