7f999cca4f64c56d582eb0c28e17d67573e6fa33
ejoongseok <ejoongseok@gmail.com>
2022-06-30 17:19:16 +0900

    상품 출고 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/stock/StockOutUsecaseTest.java
+++ src/test/java/kata/orderinhexagonal/stock/StockOutUsecaseTest.java
@@ -3,16 +3,29 @@ package kata.orderinhexagonal.stock;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import kata.orderinhexagonal.fixture.ItemFixture;
+import kata.orderinhexagonal.fixture.StockFixture;
+import kata.orderinhexagonal.item.domain.Item;
 import kata.orderinhexagonal.stock.domain.Stock;
 
 class StockOutUsecaseTest {
 
+	ItemFixture itemFixture;
+
+	StockFixture stockFixture;
+
 	@Test
 	void 상품_출고() {
 		// given
+		Item item = itemFixture.createItem("노트북", 1_000_000);
+		int stockOutQuantity = 5;
+		int stockInQuantity = 10;
+		stockFixture.stockIn(item, stockInQuantity);
+		Item stockInItem = itemFixture.getItem(item.getId());
+		int currentQuantity = stockInItem.getStockQuantity() - stockOutQuantity;
 
 		// when
-
+		Stock stockOut = stockOutUsecase.stockOut(item.getId(), stockOutQuantity);
 		// then
 		Assertions.assertThat(stockOut.getId()).isPositive();
 		Assertions.assertThat(stockOut.getStockType()).isEqualTo(Stock.StockType.STOCK_OUT);
