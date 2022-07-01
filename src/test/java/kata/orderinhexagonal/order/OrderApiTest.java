6c7a971deea5818c1c194e20215b3d1c8ec3b273
ejoongseok <ejoongseok@gmail.com>
2022-07-01 11:06:36 +0900

    상품 주문 테스트 케이스 Assertions 작성

--- src/test/java/kata/orderinhexagonal/order/OrderApiTest.java
+++ src/test/java/kata/orderinhexagonal/order/OrderApiTest.java
@@ -1,6 +1,8 @@
 package kata.orderinhexagonal.order;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 class OrderApiTest {
 
@@ -11,6 +13,16 @@ class OrderApiTest {
 		// when
 
 		// then
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+		Assertions.assertThat(CreateOrderResponse.getId()).isPositive();
+		Assertions.assertThat(CreateOrderResponse.getOrderItems()).isSize(2);
+		Assertions.assertThat(CreateOrderResponse.getOrderItems().get(0).getQuantity()).isEqualTo(orderItem1.getQuantity());
+		Assertions.assertThat(CreateOrderResponse.getOrderItems().get(0).getPrice()).isEqualTo(orderItem1TotalPrice);
+		Assertions.assertThat(CreateOrderResponse.getOrderItems().get(1).getQuantity()).isEqualTo(orderItem2.getQuantity());
+		Assertions.assertThat(CreateOrderResponse.getOrderItems().get(1).getPrice()).isEqualTo(orderItem2TotalPrice);
+		Assertions.assertThat(CreateOrderResponse.getTotalPrice()).isEqualTo(totalPrice);
+		Assertions.assertThat(CreateOrderResponse.getStatus()).isEqualTo(OrderStatus.NOT_PAYED);
+		Assertions.assertThat(CreateOrderResponse.getMember().getName()).isEqualTo(member.getName());
 	}
 
 }
