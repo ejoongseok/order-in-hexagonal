fb39152eca4ab1b3e74ea595df8c0d9b10fbb096
ejoongseok <ejoongseok@gmail.com>
2022-07-01 09:48:37 +0900

    added save method Transactional

--- src/main/java/kata/orderinhexagonal/stock/adapter/out/persistence/SaveStockAdapter.java
+++ src/main/java/kata/orderinhexagonal/stock/adapter/out/persistence/SaveStockAdapter.java
@@ -1,7 +1,11 @@
 package kata.orderinhexagonal.stock.adapter.out.persistence;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import kata.orderinhexagonal.item.adapter.out.persistence.ItemEntity;
+import kata.orderinhexagonal.item.domain.Item;
+import kata.orderinhexagonal.stock.application.port.out.LoadItemPort;
 import kata.orderinhexagonal.stock.application.port.out.SaveStockPort;
 import kata.orderinhexagonal.stock.domain.Stock;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +16,16 @@ public class SaveStockAdapter implements SaveStockPort {
 
 	private final StockRepository stockRepository;
 	private final StockMapper stockMapper;
+	private final LoadItemPort loadItemPort;
 
 	@Override
+	@Transactional
 	public void save(Stock stock) {
 		StockEntity stockEntity = stockMapper.toEntity(stock);
+		Item stockItem = stock.getItem();
+		ItemEntity itemEntity = loadItemPort.loadEntity(stockItem.getId());
+		itemEntity.changeStockQuantity(stockItem.getStockQuantity());
+		stockEntity.updateItemEntity(itemEntity);
 		stockRepository.save(stockEntity);
 		stock.assignId(stockEntity.getId());
 	}
