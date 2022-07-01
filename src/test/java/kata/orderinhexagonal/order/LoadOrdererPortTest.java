84d149e2074d0720c5c6414837c589b5ce11ab97
ejoongseok <ejoongseok@gmail.com>
2022-07-01 16:25:44 +0900

    상품 주문자 정보 불러오기 테스트 케이스 생성

---
+++ src/test/java/kata/orderinhexagonal/order/LoadOrdererPortTest.java
@@ -0,0 +1,16 @@
+package kata.orderinhexagonal.order;
+
+import org.junit.jupiter.api.Test;
+
+class LoadOrdererPortTest {
+	
+	@Test
+	void loadOrdererTest() {
+		// given
+		
+		// when
+	    
+		// then
+	}
+	
+}
