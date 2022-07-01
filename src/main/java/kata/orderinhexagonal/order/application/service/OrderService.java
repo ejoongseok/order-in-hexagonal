f8ecb238c1c4834343cd7c0a2914ca7c7c495a6e
ejoongseok <ejoongseok@gmail.com>
2022-07-01 16:30:46 +0900

    import LoadOrdererPort

--- src/main/java/kata/orderinhexagonal/order/application/service/OrderService.java
+++ src/main/java/kata/orderinhexagonal/order/application/service/OrderService.java
@@ -1,13 +1,20 @@
 package kata.orderinhexagonal.order.application.service;
 
+import org.springframework.stereotype.Service;
+
+import kata.orderinhexagonal.member.domain.Member;
 import kata.orderinhexagonal.order.application.port.in.CreateOrderRequest;
 import kata.orderinhexagonal.order.application.port.in.CreateOrderUsecase;
+import kata.orderinhexagonal.order.application.port.out.LoadOrdererPort;
 import kata.orderinhexagonal.order.domain.Order;
 
+@Service
 public class OrderService implements CreateOrderUsecase {
+	private LoadOrdererPort loadOrdererPort;
+
 	@Override
 	public Order createOrder(CreateOrderRequest request) {
-
+		Member orderer = loadOrdererPort.load(request.getOrdererId());
 		return null;
 	}
 }
