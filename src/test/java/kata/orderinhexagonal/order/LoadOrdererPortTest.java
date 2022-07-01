a131f924d12df31cc877c0c9e2df486c31a6d2b4
ejoongseok <ejoongseok@gmail.com>
2022-07-01 16:29:16 +0900

    상품 주문자 정보 불러오기 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/order/LoadOrdererPortTest.java
+++ src/test/java/kata/orderinhexagonal/order/LoadOrdererPortTest.java
@@ -1,16 +1,29 @@
 package kata.orderinhexagonal.order;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import kata.orderinhexagonal.fixture.MemberFixture;
+import kata.orderinhexagonal.member.domain.Member;
+
+@SpringBootTest
 class LoadOrdererPortTest {
-	
+
+	@Autowired
+	MemberFixture memberFixture;
+	@Autowired
+	LoadOrdererPort loadOrdererPort;
+
 	@Test
 	void loadOrdererTest() {
 		// given
-		
+		Member member = memberFixture.createMember("이중석", "ejoongseok@gmail.com", "대전광역시 서구");
 		// when
-	    
+		Member orderer = loadOrdererPort.load(member.getId());
 		// then
+		Assertions.assertThat(orderer.getId()).isEqualTo(member.getId());
 	}
 	
 }
