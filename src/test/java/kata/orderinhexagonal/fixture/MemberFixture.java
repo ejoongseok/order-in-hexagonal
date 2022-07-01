8c4c1dce339c14a069d110a368a83a7073d55071
ejoongseok <ejoongseok@gmail.com>
2022-07-01 16:13:51 +0900

    JWT 엑세스 토큰 발급 기능 추가

--- src/test/java/kata/orderinhexagonal/fixture/MemberFixture.java
+++ src/test/java/kata/orderinhexagonal/fixture/MemberFixture.java
@@ -1,7 +1,13 @@
 package kata.orderinhexagonal.fixture;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kata.orderinhexagonal.member.application.port.in.CreateMemberRequest;
 import kata.orderinhexagonal.member.application.port.in.CreateMemberUsecase;
@@ -15,5 +21,15 @@ public class MemberFixture {
 		CreateMemberRequest request = new CreateMemberRequest(name, "ejoongseok1234!", email, location);
 		Member member = createMemberUsecase.join(request);
 		return member;
+	}
+
+	public String getAccessToken(String name, String email, String location, MockMvc mockMvc, ObjectMapper objectMapper) throws
+		Exception {
+		String password = "ejoongseok1234!";
+		CreateMemberRequest request = new CreateMemberRequest(email, password, name, location);
+		return mockMvc.perform(post("/members")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(request))
+		).andReturn().getResponse().getHeader("Authorization");
 	}
 }
