a186230935d236267e2ecfe1387415d3746e14c7
ejoongseok <ejoongseok@gmail.com>
2022-07-01 14:36:09 +0900

    Jwt 토큰 생성 테스트 케이스 추가

--- src/test/java/kata/orderinhexagonal/auth/JwtAuthTest.java
+++ src/test/java/kata/orderinhexagonal/auth/JwtAuthTest.java
@@ -1,5 +1,16 @@
 package kata.orderinhexagonal.auth;
 
+import org.junit.jupiter.api.Test;
+
 class JwtAuthTest {
+
+	@Test
+	void createJwtTokenTest() {
+		// given
+
+		// when
+
+		// then
+	}
 
 }
