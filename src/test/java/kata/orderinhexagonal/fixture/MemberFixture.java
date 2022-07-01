ed802da611161fd95eb708961b6a1479e506a194
ejoongseok <ejoongseok@gmail.com>
2022-07-01 13:22:58 +0900

    added spring bean

--- src/test/java/kata/orderinhexagonal/fixture/MemberFixture.java
+++ src/test/java/kata/orderinhexagonal/fixture/MemberFixture.java
@@ -1,11 +1,15 @@
 package kata.orderinhexagonal.fixture;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import kata.orderinhexagonal.member.application.port.in.CreateMemberRequest;
 import kata.orderinhexagonal.member.application.port.in.CreateMemberUsecase;
 import kata.orderinhexagonal.member.domain.Member;
 
+@Component
 public class MemberFixture {
-	CreateMemberUsecase createMemberUsecase;
+	@Autowired CreateMemberUsecase createMemberUsecase;
 
 	public Member createMember(String name, String email, String location) {
 		CreateMemberRequest request = new CreateMemberRequest(name, "ejoongseok1234!", email, location);
