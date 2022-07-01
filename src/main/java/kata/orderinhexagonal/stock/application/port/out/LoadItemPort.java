327ef49b096c73857bcaac2a63066bfffc5add15
ejoongseok <ejoongseok@gmail.com>
2022-07-01 09:07:07 +0900

    added method loadEntity

--- src/main/java/kata/orderinhexagonal/stock/application/port/out/LoadItemPort.java
+++ src/main/java/kata/orderinhexagonal/stock/application/port/out/LoadItemPort.java
@@ -1,7 +1,10 @@
 package kata.orderinhexagonal.stock.application.port.out;
 
+import kata.orderinhexagonal.item.adapter.out.persistence.ItemEntity;
 import kata.orderinhexagonal.item.domain.Item;
 
 public interface LoadItemPort {
 	Item load(Long id);
+
+	ItemEntity loadEntity(Long id);
 }
