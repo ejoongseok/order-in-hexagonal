fe625100d9992551e3ed47721a689bf7cffac38c
ejoongseok <ejoongseok@gmail.com>
2022-07-01 09:06:38 +0900

    added loadItemEntity test case

--- src/test/java/kata/orderinhexagonal/stock/LoadItemPortTest.java
+++ src/test/java/kata/orderinhexagonal/stock/LoadItemPortTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import kata.orderinhexagonal.fixture.ItemFixture;
+import kata.orderinhexagonal.item.adapter.out.persistence.ItemEntity;
 import kata.orderinhexagonal.item.domain.Item;
 import kata.orderinhexagonal.stock.application.port.out.LoadItemPort;
 
@@ -27,5 +28,19 @@ class LoadItemPortTest {
 		Assertions.assertThat(loadItem.getPrice()).isEqualTo(item.getPrice());
 		Assertions.assertThat(loadItem.getStockQuantity()).isEqualTo(item.getStockQuantity());
 	}
+
+	@Test
+	void loadItemEntityTest() {
+		// given
+		Item item = itemFixture.createItem("노트북", 1_000_000);
+		// when
+		ItemEntity loadItemEntity = loadItemPort.loadEntity(item.getId());
+		// then
+		Assertions.assertThat(loadItemEntity.getId()).isEqualTo(item.getId());
+		Assertions.assertThat(loadItemEntity.getName()).isEqualTo(item.getName());
+		Assertions.assertThat(loadItemEntity.getPrice()).isEqualTo(item.getPrice());
+		Assertions.assertThat(loadItemEntity.getStockQuantity()).isEqualTo(item.getStockQuantity());
+	}
+
 
 }
