af580422a9371f8101efba302440a6c7ad948ac2
ejoongseok <ejoongseok@gmail.com>
2022-07-01 09:49:04 +0900

    change parameter

--- src/main/java/kata/orderinhexagonal/stock/adapter/out/persistence/StockMapper.java
+++ src/main/java/kata/orderinhexagonal/stock/adapter/out/persistence/StockMapper.java
@@ -2,16 +2,11 @@ package kata.orderinhexagonal.stock.adapter.out.persistence;
 
 import org.springframework.stereotype.Component;
 
-import kata.orderinhexagonal.item.adapter.out.persistence.ItemMapper;
 import kata.orderinhexagonal.stock.domain.Stock;
-import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
 public class StockMapper {
-
-	private final ItemMapper itemMapper;
 	public StockEntity toEntity(Stock stock) {
-		return new StockEntity(stock.getQuantity(), itemMapper.toEntity(stock.getItem()), stock.getStockType());
+		return new StockEntity(stock.getQuantity(), stock.getStockType());
 	}
 }
