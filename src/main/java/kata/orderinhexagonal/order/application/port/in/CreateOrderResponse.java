b7be831fdd5d81b2875369967fc6c1ec240b7326
ejoongseok <ejoongseok@gmail.com>
2022-07-01 13:28:21 +0900

    added lombok getter

--- src/main/java/kata/orderinhexagonal/order/application/port/in/CreateOrderResponse.java
+++ src/main/java/kata/orderinhexagonal/order/application/port/in/CreateOrderResponse.java
@@ -5,31 +5,13 @@ import java.util.List;
 
 import kata.orderinhexagonal.member.domain.Member;
 import kata.orderinhexagonal.order.domain.OrderStatus;
+import lombok.Getter;
 
+@Getter
 public class CreateOrderResponse {
 	private Long id;
 	private List<OrderItemResponse> orderItems = new ArrayList<>();
 	private int totalPrice;
 	private OrderStatus status;
 	private Member member;
-
-	public Long getId() {
-		return id;
-	}
-
-	public List<OrderItemResponse> getOrderItems() {
-		return orderItems;
-	}
-
-	public int getTotalPrice() {
-		return totalPrice;
-	}
-
-	public OrderStatus getStatus() {
-		return status;
-	}
-
-	public Member getMember() {
-		return member;
-	}
 }
--- src/main/java/kata/orderinhexagonal/order/application/port/in/OrderItemResponse.java
+++ src/main/java/kata/orderinhexagonal/order/application/port/in/OrderItemResponse.java
@@ -1,19 +1,10 @@
 package kata.orderinhexagonal.order.application.port.in;
 
+import lombok.Getter;
+
+@Getter
 public class OrderItemResponse {
 	private long itemId;
 	private int quantity;
 	private int price;
-
-	public long getItemId() {
-		return itemId;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public int getPrice() {
-		return price;
-	}
 }
