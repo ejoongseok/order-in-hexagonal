2627ba0bd0623faec9237bc8f4c0409e24b33a8c
ejoongseok <ejoongseok@gmail.com>
2022-07-04 09:43:54 +0900

    added Spring boot test

--- src/test/java/kata/orderinhexagonal/order/LoadOrderItemPortTest.java
+++ src/test/java/kata/orderinhexagonal/order/LoadOrderItemPortTest.java
@@ -2,15 +2,22 @@ package kata.orderinhexagonal.order;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import kata.orderinhexagonal.fixture.ItemFixture;
 import kata.orderinhexagonal.item.domain.Item;
 
+@SpringBootTest
 class LoadOrderItemPortTest {
+
+	@Autowired ItemFixture itemFixture;
+	@Autowired LoadOrderItemPort loadOrderItemPort;
 
 	@Test
 	void loadItemTest() {
 		// given
-		Item createItem = itemFixture.createItem();
+		Item createItem = itemFixture.createItem("가방", 100_000);
 		// when
 		Item loadItem = loadOrderItemPort.loadItem(createItem.getId());
 
