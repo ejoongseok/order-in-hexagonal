dabf8c2fe7c277b1c545ef4eeaea719fc1b278f6
ejoongseok <ejoongseok@gmail.com>
2022-07-04 09:42:36 +0900

    상품 불러오기 아웃바운드 포트 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/order/LoadOrderItemPortTest.java
+++ src/test/java/kata/orderinhexagonal/order/LoadOrderItemPortTest.java
@@ -1,4 +1,24 @@
 package kata.orderinhexagonal.order;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import kata.orderinhexagonal.item.domain.Item;
+
 class LoadOrderItemPortTest {
+
+	@Test
+	void loadItemTest() {
+		// given
+		Item createItem = itemFixture.createItem();
+		// when
+		Item loadItem = loadOrderItemPort.loadItem(createItem.getId());
+
+		// then
+		Assertions.assertThat(loadItem).isNotNull();
+		Assertions.assertThat(loadItem.getId()).isEqualTo(createItem.getId());
+		Assertions.assertThat(loadItem.getName()).isEqualTo(createItem.getName());
+		Assertions.assertThat(loadItem.getPrice()).isEqualTo(createItem.getPrice());
+	}
+
 }
