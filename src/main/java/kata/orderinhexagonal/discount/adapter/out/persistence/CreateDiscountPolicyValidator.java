13670da5cc98ac0a5fbae9d93a27e89f76790bf2
ejoongseok <ejoongseok@gmail.com>
2022-07-04 17:30:22 +0900

    할인 정책을 적용하려는 상품에 이미 존재하는 할인 정책이 있는지 확인 테스트 생성

---
+++ src/main/java/kata/orderinhexagonal/discount/adapter/out/persistence/CreateDiscountPolicyValidator.java
@@ -0,0 +1,5 @@
+package kata.orderinhexagonal.discount.adapter.out.persistence;
+
+public interface CreateDiscountPolicyValidator {
+	void existsDiscountItemCheck(Long id);
+}
---
+++ src/test/java/kata/orderinhexagonal/discount/CreateDiscountPolicyValidatorTest.java
@@ -0,0 +1,16 @@
+package kata.orderinhexagonal.discount;
+
+import org.junit.jupiter.api.Test;
+
+class CreateDiscountPolicyValidatorTest {
+
+	@Test
+	void existsDiscountItemCheckTest() {
+		// given
+
+		// when
+
+		// then
+	}
+
+}
