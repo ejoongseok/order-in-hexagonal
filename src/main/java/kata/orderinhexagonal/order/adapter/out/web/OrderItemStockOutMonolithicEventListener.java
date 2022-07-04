b49e0d53fd7bca03ce6729128b22e5d2a6e55c04
ejoongseok <ejoongseok@gmail.com>
2022-07-04 10:51:36 +0900

    item 도메인 주문 차감 로직 추가

--- src/main/java/kata/orderinhexagonal/order/adapter/out/web/OrderItemStockOutMonolithicEventListener.java
+++ src/main/java/kata/orderinhexagonal/order/adapter/out/web/OrderItemStockOutMonolithicEventListener.java
@@ -4,6 +4,8 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import kata.orderinhexagonal.item.domain.Item;
+import kata.orderinhexagonal.stock.application.port.in.StockOutRequest;
 import kata.orderinhexagonal.stock.application.port.in.StockOutUsecase;
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +18,8 @@ public class OrderItemStockOutMonolithicEventListener {
 	@Async
 	@EventListener
 	public void handle(OrderItemStockOutEvent event) {
-		stockOutUsecase.stockOut(event.getStockOutRequest());
+		Item item = event.getItem();
+		item.stockOutQuantity(event.getStockOutQuantity());
+		stockOutUsecase.stockOut(StockOutRequest.of(item.getId(), event.getStockOutQuantity()));
 	}
 }
