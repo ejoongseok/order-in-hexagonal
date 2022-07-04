f65f2a7a2b629e79d20de554438ee32f53845d23
ejoongseok <ejoongseok@gmail.com>
2022-07-04 17:17:05 +0900

    할인 정책 적용 상품 불러오기 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/discount/DiscountItemLoadPortTest.java
+++ src/test/java/kata/orderinhexagonal/discount/DiscountItemLoadPortTest.java
@@ -1,4 +1,33 @@
 package kata.orderinhexagonal.discount;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kata.orderinhexagonal.discount.application.port.out.DiscountItemLoadPort;
+import kata.orderinhexagonal.fixture.ItemFixture;
+import kata.orderinhexagonal.item.domain.Item;
+
+@SpringBootTest
 class DiscountItemLoadPortTest {
+
+	@Autowired
+	ItemFixture itemFixture;
+	@Autowired DiscountItemLoadPort discountItemLoad;
+
+	@Test
+	void discountItemLoadTest() {
+		// given
+		Item createItem = itemFixture.createItem("노트북", 1_000_000);
+		// when
+	    Item loadItem = discountItemLoad.load(createItem.getId());
+		// then
+		Assertions.assertThat(loadItem.getId()).isPositive();
+		Assertions.assertThat(loadItem.getId()).isEqualTo(createItem.getId());
+		Assertions.assertThat(loadItem.getName()).isEqualTo(createItem.getName());
+		Assertions.assertThat(loadItem.getPrice()).isEqualTo(createItem.getPrice());
+		Assertions.assertThat(loadItem.getStockQuantity()).isEqualTo(createItem.getStockQuantity());
+	}
+
 }
