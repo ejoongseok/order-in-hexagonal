baa3516f686f5cb64ef9b74b34783b516912bee2
ejoongseok <ejoongseok@gmail.com>
2022-07-04 17:32:45 +0900

    할인 정책을 적용하려는 상품에 이미 존재하는 할인 정책이 있는지 확인 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/discount/CreateDiscountPolicyValidatorTest.java
+++ src/test/java/kata/orderinhexagonal/discount/CreateDiscountPolicyValidatorTest.java
@@ -1,16 +1,30 @@
 package kata.orderinhexagonal.discount;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import kata.orderinhexagonal.discount.adapter.out.persistence.CreateDiscountPolicyValidator;
+import kata.orderinhexagonal.fixture.ItemFixture;
+import kata.orderinhexagonal.item.domain.Item;
+
+@SpringBootTest
 class CreateDiscountPolicyValidatorTest {
 
+	@Autowired CreateDiscountPolicyValidator createDiscountPolicyValidator;
+	@Autowired
+	ItemFixture itemFixture;
 	@Test
 	void existsDiscountItemCheckTest() {
 		// given
+		Item item = itemFixture.createItem("노트북", 1_000_000);
 
 		// when
+		boolean result = createDiscountPolicyValidator.existsDiscountItemCheck(item.getId());
 
 		// then
+		Assertions.assertThat(result).isFalse();
 	}
 
 }
