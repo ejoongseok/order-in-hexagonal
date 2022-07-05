abb1186cbac8b7c6beeac25f779766d2672522c0
ejoongseok <ejoongseok@gmail.com>
2022-07-05 09:25:21 +0900

    상품 할인 정책 등록 기능 CreateItemDiscountResponse 반환하도록 변경

--- src/main/java/kata/orderinhexagonal/discount/adapter/in/web/DiscountController.java
+++ src/main/java/kata/orderinhexagonal/discount/adapter/in/web/DiscountController.java
@@ -9,14 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kata.orderinhexagonal.discount.application.port.in.CreateItemDiscountRequest;
 import kata.orderinhexagonal.discount.application.port.in.CreateItemDiscountResponse;
+import kata.orderinhexagonal.discount.application.port.in.CreateItemDiscountUsecase;
+import kata.orderinhexagonal.discount.domain.Discount;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/discounts")
+@RequiredArgsConstructor
 public class DiscountController {
+
+	private final CreateItemDiscountUsecase createItemDiscountUsecase;
 
 	@PostMapping("/items")
 	@ResponseStatus(HttpStatus.CREATED)
 	public CreateItemDiscountResponse createDiscount(@RequestBody CreateItemDiscountRequest request) {
-		return null;
+		Discount itemDiscount = createItemDiscountUsecase.createItemDiscount(request);
+		return new CreateItemDiscountResponse(itemDiscount.getId(), itemDiscount.getItem().getId(), itemDiscount.getDiscountType(), itemDiscount.getDiscountValue());
 	}
 }
