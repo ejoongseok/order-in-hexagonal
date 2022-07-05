48ed3df14a7b85ab1aab19c78a92ab646dfeef18
ejoongseok <ejoongseok@gmail.com>
2022-07-05 15:57:26 +0900

    added method createOrder

--- src/test/java/kata/orderinhexagonal/fixture/OrderFixture.java
+++ src/test/java/kata/orderinhexagonal/fixture/OrderFixture.java
@@ -1,10 +1,17 @@
 package kata.orderinhexagonal.fixture;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import kata.orderinhexagonal.item.domain.Item;
 import kata.orderinhexagonal.order.adapter.out.persistence.OrderEntity;
 import kata.orderinhexagonal.order.adapter.out.persistence.OrderRepository;
+import kata.orderinhexagonal.order.application.port.in.CreateOrderRequest;
+import kata.orderinhexagonal.order.application.port.in.CreateOrderUsecase;
+import kata.orderinhexagonal.order.application.port.in.OrderItemRequest;
+import kata.orderinhexagonal.order.domain.Order;
 
 @Component
 public class OrderFixture {
@@ -12,11 +19,30 @@ public class OrderFixture {
 	@Autowired
 	OrderRepository orderRepository;
 
+	@Autowired
+	CreateOrderUsecase createOrderUsecase;
+
+	@Autowired
+	ItemFixture itemFixture;
+
+	@Autowired
+	StockFixture stockFixture;
+
 	public OrderEntity getOrderEntity(Long id) {
 		return orderRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Order not found"));
 	}
 
 	public void clearOrder() {
 		orderRepository.deleteAll();
+	}
+
+	public Order createOrder(long ordererId) {
+		Item item = itemFixture.createItem("노트북", 1_000_000);
+		stockFixture.stockIn(item, 10);
+		CreateOrderRequest request = CreateOrderRequest.of(List.of(
+			OrderItemRequest.of(item.getId(), 1)
+		));
+		request.assignOrdererId(ordererId);
+		return createOrderUsecase.createOrder(request);
 	}
 }
