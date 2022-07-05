52a1be8d5bebfea3c8bd39ff5aa0fd5d6be3fbfc
ejoongseok <ejoongseok@gmail.com>
2022-07-05 10:27:10 +0900

    할인 정책 필드 추가

--- src/main/java/kata/orderinhexagonal/item/adapter/out/persistence/ItemMapper.java
+++ src/main/java/kata/orderinhexagonal/item/adapter/out/persistence/ItemMapper.java
@@ -2,15 +2,18 @@ package kata.orderinhexagonal.item.adapter.out.persistence;
 
 import org.springframework.stereotype.Component;
 
+import kata.orderinhexagonal.discount.adapter.out.persistence.DiscountMapper;
 import kata.orderinhexagonal.item.domain.Item;
+import lombok.RequiredArgsConstructor;
 
 @Component
 public class ItemMapper {
 
+
 	public ItemEntity toEntity(Item domain) {
-		return new ItemEntity(domain.getId(), domain.getName(), domain.getPrice(), domain.getStockQuantity());
+		return new ItemEntity(domain.getId(), domain.getName(), domain.getPrice(), domain.getStockQuantity(), domain.getDiscount());
 	}
 	public Item toDomain(ItemEntity entity) {
-		return new Item(entity.getId(), entity.getName(), entity.getPrice(), entity.getStockQuantity());
+		return new Item(entity.getId(), entity.getName(), entity.getPrice(), entity.getStockQuantity(), entity.getDiscount());
 	}
 }
