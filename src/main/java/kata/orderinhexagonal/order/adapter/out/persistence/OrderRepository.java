1c8a8e9de9e06dcb3d4d37df5db38a3d20d31a36
ejoongseok <ejoongseok@gmail.com>
2022-07-05 16:56:51 +0900

    added method findOrderWithMemberById

--- src/main/java/kata/orderinhexagonal/order/adapter/out/persistence/OrderRepository.java
+++ src/main/java/kata/orderinhexagonal/order/adapter/out/persistence/OrderRepository.java
@@ -1,6 +1,11 @@
 package kata.orderinhexagonal.order.adapter.out.persistence;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+	@Query("select o from OrderEntity o join fetch o.member where o.id = ?1")
+	Optional<OrderEntity> findOrderWithMemberById(Long orderId);
 }
