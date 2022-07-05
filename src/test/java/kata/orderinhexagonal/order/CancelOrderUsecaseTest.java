e92d2162473a5d4205c6cdb3304465d820d2e386
ejoongseok <ejoongseok@gmail.com>
2022-07-05 16:17:44 +0900

    주문 취소 인바운드 포트 테스트 생성

---
+++ src/test/java/kata/orderinhexagonal/order/CancelOrderUsecaseTest.java
@@ -0,0 +1,16 @@
+package kata.orderinhexagonal.order;
+
+import org.junit.jupiter.api.Test;
+
+class CancelOrderUsecaseTest {
+
+	@Test
+	void 주문_취소() {
+		// given
+
+		// when
+
+		// then
+	}
+
+}
