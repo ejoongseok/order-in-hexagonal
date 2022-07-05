8128f537a8f5830b3126d1d4a5de674d003c1453
ejoongseok <ejoongseok@gmail.com>
2022-07-05 16:23:35 +0900

    주문 취소 인바운드 포트 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/order/CancelOrderUsecaseTest.java
+++ src/test/java/kata/orderinhexagonal/order/CancelOrderUsecaseTest.java
@@ -1,16 +1,36 @@
 package kata.orderinhexagonal.order;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import kata.orderinhexagonal.fixture.MemberFixture;
+import kata.orderinhexagonal.fixture.OrderFixture;
+import kata.orderinhexagonal.member.domain.Member;
+import kata.orderinhexagonal.order.application.port.in.CancelOrderRequest;
+import kata.orderinhexagonal.order.domain.Order;
+import kata.orderinhexagonal.order.domain.OrderStatus;
+
+@SpringBootTest
 class CancelOrderUsecaseTest {
+
+	@Autowired
+	OrderFixture orderFixture;
+	@Autowired
+	MemberFixture memberFixture;
 
 	@Test
 	void 주문_취소() {
 		// given
-
+		Member orderer = memberFixture.createMember("이중석", "ejoongseok@gmail.com", "대전광역시 서구");
+		Order createOrder = orderFixture.createOrder(orderer.getId());
+		CancelOrderRequest request = CancelOrderRequest.of(createOrder.getId());
 		// when
-
+		Order cancelOrder = cancelOrderUsecase.cancelOrder(request);
 		// then
+		Assertions.assertThat(cancelOrder.getId()).isEqualTo(createOrder.getId());
+		Assertions.assertThat(cancelOrder.getStatus()).isEqualTo(OrderStatus.CANCELED);
 	}
 
 }
