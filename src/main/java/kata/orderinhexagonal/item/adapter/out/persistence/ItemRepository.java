485a554c9d6f2459d52060c616e07e80d338ac7c
ejoongseok <ejoongseok@gmail.com>
2022-07-05 10:25:07 +0900

    아이템 도메인 조회 시 할인 정책 페치 조인

--- src/main/java/kata/orderinhexagonal/item/adapter/out/persistence/ItemRepository.java
+++ src/main/java/kata/orderinhexagonal/item/adapter/out/persistence/ItemRepository.java
@@ -1,6 +1,11 @@
 package kata.orderinhexagonal.item.adapter.out.persistence;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
+	@Query("select i from ItemEntity i left join fetch i.discount where i.id = ?1")
+	Optional<ItemEntity> findItemAndDiscountFetchById(Long id);
 }
