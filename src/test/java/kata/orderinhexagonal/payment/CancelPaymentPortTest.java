a6bbf33a413853dc35ed6dded82cecc1b4bc8496
ejoongseok <ejoongseok@gmail.com>
2022-07-06 16:53:38 +0900

    결제 취소 테스트 케이스 생성

--- src/test/java/kata/orderinhexagonal/payment/CancelPaymentPortTest.java
+++ src/test/java/kata/orderinhexagonal/payment/CancelPaymentPortTest.java
@@ -1,5 +1,17 @@
 package kata.orderinhexagonal.payment;
 
-public class CancelPaymentPortTest {
+import org.junit.jupiter.api.Test;
+
+class CancelPaymentPortTest {
+
+	@Test
+	void 결제_취소() {
+		// given
+
+		// when
+
+		// then
+	}
+
 
 }
