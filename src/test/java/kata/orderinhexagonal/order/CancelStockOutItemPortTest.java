1dd8d74c8afd1045d199ba0241c8b4b3314e74e6
ejoongseok <ejoongseok@gmail.com>
2022-07-06 09:22:44 +0900

    상품 출고 취소 테스트 케이스 생성

--- src/test/java/kata/orderinhexagonal/order/CancelStockOutItemPortTest.java
+++ src/test/java/kata/orderinhexagonal/order/CancelStockOutItemPortTest.java
@@ -1,4 +1,16 @@
 package kata.orderinhexagonal.order;
 
+import org.junit.jupiter.api.Test;
+
 class CancelStockOutItemPortTest {
+
+	@Test
+	void 상품_출고_취소() {
+		// given
+
+		// when
+
+		// then
+	}
+
 }
