0d2c93bc5aabf7aa931aaa41c265a35c8f40cb75
ejoongseok <ejoongseok@gmail.com>
2022-07-06 14:16:38 +0900

    결제 주문 정보 불러오기 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/payment/OrderLoadPortTest.java
+++ src/test/java/kata/orderinhexagonal/payment/OrderLoadPortTest.java
@@ -1,16 +1,41 @@
 package kata.orderinhexagonal.payment;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import kata.orderinhexagonal.discount.adapter.out.web.DiscountItemLoadAdapter;
+import kata.orderinhexagonal.fixture.MemberFixture;
+import kata.orderinhexagonal.fixture.OrderFixture;
+import kata.orderinhexagonal.member.domain.Member;
+import kata.orderinhexagonal.order.domain.Order;
+
+@SpringBootTest
 class OrderLoadPortTest {
+
+	@Autowired
+	MemberFixture memberFixture;
+
+	@Autowired
+	OrderFixture orderFixture;
+
+	@Autowired
+	OrderLoadPort orderLoadPort;
 
 	@Test
 	void 주문정보불러오기() {
 		// given
-
+		Member member = memberFixture.createMember("이중석", "ejoongseok@gmail.com", "대전광역시 서구");
+		Order order = orderFixture.createOrder(member.getId());
 		// when
+		Order loadOrder = orderLoadPort.load(order.getId());
 
 		// then
+		Assertions.assertThat(loadOrder).isNotNull();
+		Assertions.assertThat(loadOrder.getMember()).isNotNull();
+		Assertions.assertThat(loadOrder.getOrderItems()).isNotEmpty();
+		Assertions.assertThat(loadOrder.getOrderItems()).hasSize(1);
 	}
 
 }
