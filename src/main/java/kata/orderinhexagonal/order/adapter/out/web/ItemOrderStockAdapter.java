c1ffc744173c00c9a3daae29107b0a114db71e79
ejoongseok <ejoongseok@gmail.com>
2022-07-06 09:30:58 +0900

    implements CancelStockOutItemPort

--- src/main/java/kata/orderinhexagonal/order/adapter/out/web/ItemOrderStockAdapter.java
+++ src/main/java/kata/orderinhexagonal/order/adapter/out/web/ItemOrderStockAdapter.java
@@ -3,17 +3,24 @@ package kata.orderinhexagonal.order.adapter.out.web;
 import org.springframework.stereotype.Component;
 
 import kata.orderinhexagonal.item.domain.Item;
+import kata.orderinhexagonal.order.application.port.out.CancelStockOutItemPort;
 import kata.orderinhexagonal.order.application.port.out.ItemOrderStockOutPort;
+import kata.orderinhexagonal.stock.domain.Stock;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class ItemOrderStockAdapter implements ItemOrderStockOutPort {
+public class ItemOrderStockAdapter implements ItemOrderStockOutPort, CancelStockOutItemPort {
 
 	private final ItemOrderStockNetworkClient itemOrderStockNetworkClient;
 
 	@Override
 	public void stockOut(Item item, int stockOutQuantity) {
 		itemOrderStockNetworkClient.stockOut(item, stockOutQuantity);
+	}
+
+	@Override
+	public Stock cancelStockOutItem(Item cancelOrderItem, int orderQuantity) {
+		return itemOrderStockNetworkClient.stockIn(cancelOrderItem, orderQuantity);
 	}
 }
