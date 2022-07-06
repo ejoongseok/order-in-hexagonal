a38e926448655673eb762045bb91f11f03a08a3c
ejoongseok <ejoongseok@gmail.com>
2022-07-06 10:33:30 +0900

    added lombok

--- src/main/java/kata/orderinhexagonal/order/application/port/in/CancelOrderResponse.java
+++ src/main/java/kata/orderinhexagonal/order/application/port/in/CancelOrderResponse.java
@@ -1,9 +1,16 @@
 package kata.orderinhexagonal.order.application.port.in;
 
+import kata.orderinhexagonal.order.domain.Order;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CancelOrderResponse {
 	private Long orderId;
 
-	public Long getOrderId() {
-		return orderId;
+	public CancelOrderResponse(Order cancelOrder) {
+		this.orderId = cancelOrder.getId();
 	}
 }
