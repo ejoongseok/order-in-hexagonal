d8ac31bdfc0f56b61f631dd49a47ba75bd82b0fe
ejoongseok <ejoongseok@gmail.com>
2022-07-06 13:46:14 +0900

    주문 결제 테스트 케이스 Assertions 작성

--- src/test/java/kata/orderinhexagonal/payment/PaymentUsecaseTest.java
+++ src/test/java/kata/orderinhexagonal/payment/PaymentUsecaseTest.java
@@ -1,6 +1,12 @@
 package kata.orderinhexagonal.payment;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import kata.orderinhexagonal.order.domain.OrderStatus;
+import kata.orderinhexagonal.payment.domain.CardCompany;
+import kata.orderinhexagonal.payment.domain.CardType;
+import kata.orderinhexagonal.payment.domain.PaymentType;
 
 class PaymentUsecaseTest {
 
@@ -11,6 +17,16 @@ class PaymentUsecaseTest {
 		// when
 
 		// then
+		Assertions.assertThat(payment.getId()).isPositive();
+		Assertions.assertThat(payment.getOrderId()).isEqualTo(order.getId());
+		Assertions.assertThat(payment.getPaymentPrice()).isEqualTo(order.getTotalPrice());
+		Assertions.assertThat(payment.getCardType()).isEqualTo(CardType.CREDIT_CARD);
+		Assertions.assertThat(payment.getCardCompany()).isEqualTo(CardCompany.KATA);
+		Assertions.assertThat(payment.getPaymentType()).isEqualTo(PaymentType.PAY_IN_FULL);
+		Assertions.assertThat(payment.getCardNumber()).isEqualTo(cardNumber);
+		Assertions.assertThat(payment.getCardCvc()).isEqualTo(cardCvc);
+		Assertions.assertThat(order.getStatus()).isEqualTo(OrderStatus.PAYED);
+		Assertions.assertThat(orderEntity.getStatus()).isEqualTo(OrderStatus.PAYED);
 	}
 
 }
