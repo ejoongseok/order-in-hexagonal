bc4a5131324d550ca030aa97e14ec5c40ed34af1
ejoongseok <ejoongseok@gmail.com>
2022-07-06 17:00:28 +0900

    added method createPayment

--- src/test/java/kata/orderinhexagonal/fixture/PaymentFixture.java
+++ src/test/java/kata/orderinhexagonal/fixture/PaymentFixture.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Component;
 
 import kata.orderinhexagonal.payment.adapter.out.persistence.PaymentEntity;
 import kata.orderinhexagonal.payment.adapter.out.persistence.PaymentRepository;
+import kata.orderinhexagonal.payment.application.port.in.PaymentRequest;
+import kata.orderinhexagonal.payment.application.port.in.PaymentUsecase;
+import kata.orderinhexagonal.payment.domain.CardCompany;
+import kata.orderinhexagonal.payment.domain.CardType;
+import kata.orderinhexagonal.payment.domain.Payment;
+import kata.orderinhexagonal.payment.domain.PaymentType;
 
 @Component
 public class PaymentFixture {
@@ -12,8 +18,17 @@ public class PaymentFixture {
 	@Autowired
 	private PaymentRepository paymentRepository;
 
+	@Autowired
+	PaymentUsecase paymentUsecase;
+
 	public void clearPayment() {
 		paymentRepository.deleteAll();
+	}
+
+	public Payment createPayment(Long orderId) {
+		PaymentRequest request = PaymentRequest.of(orderId, CardType.CREDIT_CARD, CardCompany.KATA, "1234567890123456",
+			"123", PaymentType.PAY_IN_FULL);
+		return paymentUsecase.payments(request);
 	}
 
 	public PaymentEntity getPaymentEntity(Long id) {
