d0d85a84b98e7770d321d1244788441746eec88e
ejoongseok <ejoongseok@gmail.com>
2022-07-06 11:02:08 +0900

    주문 결제 테스트 케이스 Assertions 작성

--- src/test/java/kata/orderinhexagonal/payment/PaymentApiTest.java
+++ src/test/java/kata/orderinhexagonal/payment/PaymentApiTest.java
@@ -1,6 +1,12 @@
 package kata.orderinhexagonal.payment;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import kata.orderinhexagonal.order.adapter.out.persistence.OrderEntity;
+import kata.orderinhexagonal.order.domain.Order;
+import kata.orderinhexagonal.order.domain.OrderStatus;
 
 class PaymentApiTest {
 
@@ -11,6 +17,15 @@ class PaymentApiTest {
 		// when
 
 		// then
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK);
+		Assertions.assertThat(paymentResponse.getId()).isPositive();
+		Assertions.assertThat(paymentResponse.getOrderId()).isEqualTo(order.getId());
+		OrderEntity orderEntity = orderFixture.getOrderEntity(order.getId());
+		Assertions.assertThat(orderEntity.getStatus()).isEqualTo(OrderStatus.PAYED);
+		Order getOrder = orderFixture.getOrder(order.getId());
+		Assertions.assertThat(paymentResponse.getPaymentPrice()).isEqualTo(getOrder.getTotalPrice());
+		Assertions.assertThat(paymentResponse.getPaymentType()).isEqualTo(PaymentType.PAY_IN_FULL);
+		Assertions.assertThat(paymentResponse.getCardType()).isEqualTo(CardType.CREDIT_CARD);
 	}
 
 }
