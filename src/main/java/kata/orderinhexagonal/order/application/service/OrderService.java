50f96e567e1a1acaf3d04db252a78f25ec8dd37f
ejoongseok <ejoongseok@gmail.com>
2022-07-06 10:30:52 +0900

    주문 취소 로직 작성 (환불 미구현)

--- src/main/java/kata/orderinhexagonal/order/application/service/OrderService.java
+++ src/main/java/kata/orderinhexagonal/order/application/service/OrderService.java
@@ -10,8 +10,11 @@ import kata.orderinhexagonal.order.application.port.in.CancelOrderRequest;
 import kata.orderinhexagonal.order.application.port.in.CancelOrderUsecase;
 import kata.orderinhexagonal.order.application.port.in.CreateOrderRequest;
 import kata.orderinhexagonal.order.application.port.in.CreateOrderUsecase;
+import kata.orderinhexagonal.order.application.port.out.CancelOrderPort;
+import kata.orderinhexagonal.order.application.port.out.CancelStockOutItemPort;
 import kata.orderinhexagonal.order.application.port.out.ItemOrderStockOutPort;
 import kata.orderinhexagonal.order.application.port.out.LoadOrderItemPort;
+import kata.orderinhexagonal.order.application.port.out.LoadOrderPort;
 import kata.orderinhexagonal.order.application.port.out.LoadOrdererPort;
 import kata.orderinhexagonal.order.application.port.out.SaveOrderPort;
 import kata.orderinhexagonal.order.domain.Order;
@@ -24,8 +27,10 @@ public class OrderService implements CreateOrderUsecase, CancelOrderUsecase {
 	private final LoadOrdererPort loadOrdererPort;
 	private final LoadOrderItemPort loadOrderItemPort;
 	private final ItemOrderStockOutPort itemOrderStockOutPort;
-
 	private final SaveOrderPort saveOrderPort;
+	private final LoadOrderPort loadOrderPort;
+	private final CancelStockOutItemPort cancelStockOutItemPort;
+	private final CancelOrderPort cancelOrderPort;
 
 	@Override
 	public Order createOrder(CreateOrderRequest request) {
@@ -46,6 +51,22 @@ public class OrderService implements CreateOrderUsecase, CancelOrderUsecase {
 
 	@Override
 	public Order cancelOrder(CancelOrderRequest request) {
-		return null;
+		Member cancelRequester = loadOrdererPort.load(request.getOrdererId());
+		Order order = loadOrderPort.loadOrder(request.getOrderId());
+		if (!order.isOrdererAndTheRequesterMatch(cancelRequester)) {
+			throw new IllegalArgumentException("주문한 사람과 취소 요청자가 다릅니다.");
+		}
+		if (order.isDeliverd()) {
+			throw new IllegalArgumentException("배송이 시작되어 주문을 취소 할 수 없습니다.");
+		}
+		if (order.isPayed()) {
+			// 환불 요청
+		}
+		List<OrderItem> orderItems = order.getOrderItems();
+		for (OrderItem orderItem : orderItems) {
+			cancelStockOutItemPort.cancelStockOutItem(orderItem.getItem(), orderItem.getOrderQuantity());
+		}
+		cancelOrderPort.cancel(order);
+		return order;
 	}
 }
