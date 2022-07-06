fadf7c99dbb62f89478e13552cee79080d0f5363
ejoongseok <ejoongseok@gmail.com>
2022-07-06 10:20:38 +0900

    주문 취소 아웃바운드 포트 테스트 케이스 작성

--- src/test/java/kata/orderinhexagonal/order/CancelOrderPortTest.java
+++ src/test/java/kata/orderinhexagonal/order/CancelOrderPortTest.java
@@ -1,4 +1,38 @@
 package kata.orderinhexagonal.order;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kata.orderinhexagonal.fixture.MemberFixture;
+import kata.orderinhexagonal.fixture.OrderFixture;
+import kata.orderinhexagonal.member.domain.Member;
+import kata.orderinhexagonal.order.application.port.out.CancelOrderPort;
+import kata.orderinhexagonal.order.domain.Order;
+import kata.orderinhexagonal.order.domain.OrderStatus;
+
+@SpringBootTest
 class CancelOrderPortTest {
+
+	@Autowired
+	OrderFixture orderFixture;
+
+	@Autowired
+	MemberFixture memberFixture;
+
+	@Autowired
+	CancelOrderPort cancelOrderPort;
+
+	@Test
+	void cancelOrderTest() {
+		// given
+		Member member = memberFixture.createMember("이중석", "ejoongseok@gmail.com", "대전광역시 서구");
+		Order cancelOrder = orderFixture.createOrder(member.getId());
+		// when
+	    cancelOrderPort.cancel(cancelOrder);
+		// then
+		Assertions.assertThat(cancelOrder.getStatus()).isEqualTo(OrderStatus.CANCELED);
+	}
+
 }
