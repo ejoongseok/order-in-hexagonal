563d538dbf28609d7efc7df6500479927ab8113b
ejoongseok <ejoongseok@gmail.com>
2022-07-06 17:38:17 +0900

    added method findByOrderId

--- src/main/java/kata/orderinhexagonal/payment/adapter/out/persistence/PaymentRepository.java
+++ src/main/java/kata/orderinhexagonal/payment/adapter/out/persistence/PaymentRepository.java
@@ -1,6 +1,12 @@
 package kata.orderinhexagonal.payment.adapter.out.persistence;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+
+	@Query("select p from PaymentEntity p where p.order.id = ?1")
+	Optional<PaymentEntity> findByOrderId(Long orderId);
 }
