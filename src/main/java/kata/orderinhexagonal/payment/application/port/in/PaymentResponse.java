28a2ce486c172e6ec48fa653be181dd9e00baab9
ejoongseok <ejoongseok@gmail.com>
2022-07-06 16:11:30 +0900

    create Constructor

--- src/main/java/kata/orderinhexagonal/payment/application/port/in/PaymentResponse.java
+++ src/main/java/kata/orderinhexagonal/payment/application/port/in/PaymentResponse.java
@@ -2,13 +2,24 @@ package kata.orderinhexagonal.payment.application.port.in;
 
 import kata.orderinhexagonal.payment.domain.CardType;
 import kata.orderinhexagonal.payment.domain.PaymentType;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PaymentResponse {
 	private Long id;
 	private Long orderId;
 	private int paymentPrice;
 	private PaymentType paymentType;
 	private CardType cardType;
+
+	public PaymentResponse(Long id, Long orderId, int paymentPrice, PaymentType paymentType, CardType cardType) {
+		this.id = id;
+		this.orderId = orderId;
+		this.paymentPrice = paymentPrice;
+		this.paymentType = paymentType;
+		this.cardType = cardType;
+	}
 }
