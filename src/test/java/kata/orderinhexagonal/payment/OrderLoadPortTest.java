dd416d49aab042e79153d47611f7a9855d1098dc
ejoongseok <ejoongseok@gmail.com>
2022-07-06 14:13:18 +0900

    결제 주문 정보 불러오기 테스트 케이스 생성

--- src/test/java/kata/orderinhexagonal/payment/OrderLoadPortTest.java
+++ src/test/java/kata/orderinhexagonal/payment/OrderLoadPortTest.java
@@ -1,4 +1,16 @@
 package kata.orderinhexagonal.payment;
 
+import org.junit.jupiter.api.Test;
+
 class OrderLoadPortTest {
+
+	@Test
+	void 주문정보불러오기() {
+		// given
+
+		// when
+
+		// then
+	}
+
 }
