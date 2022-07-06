72be40cffce86ebde93f394488b148cb4ace184a
ejoongseok <ejoongseok@gmail.com>
2022-07-06 14:33:22 +0900

    지불 요청 테스트 케이스 생성

--- src/test/java/kata/orderinhexagonal/payment/RequestPayPortTest.java
+++ src/test/java/kata/orderinhexagonal/payment/RequestPayPortTest.java
@@ -1,4 +1,16 @@
 package kata.orderinhexagonal.payment;
 
+import org.junit.jupiter.api.Test;
+
 class RequestPayPortTest {
+	
+	@Test
+	void requestPayTest() {
+		// given
+		
+		// when
+	    
+		// then
+	}
+	
 }
