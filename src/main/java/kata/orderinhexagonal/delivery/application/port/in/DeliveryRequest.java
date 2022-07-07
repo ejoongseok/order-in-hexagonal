9a23ee3ee1499704912e7b7545e09ff2fb930da3
ejoongseok <ejoongseok@gmail.com>
2022-07-07 11:06:00 +0900

    added validation

--- src/main/java/kata/orderinhexagonal/delivery/application/port/in/DeliveryRequest.java
+++ src/main/java/kata/orderinhexagonal/delivery/application/port/in/DeliveryRequest.java
@@ -1,12 +1,17 @@
 package kata.orderinhexagonal.delivery.application.port.in;
 
+import javax.validation.constraints.NotNull;
+
 import kata.orderinhexagonal.delivery.domain.DeliveryStatus;
 import lombok.Getter;
 
 @Getter
 public class DeliveryRequest {
+	@NotNull(message = "주문 번호를 입력해주세요.")
 	private Long orderId;
+	@NotNull(message = "배송 상태를 입력해주세요.")
 	private DeliveryStatus deliveryStatus;
+	@NotNull(message = "배송 위치를 입력해주세요.")
 	private String location;
 
 	public DeliveryRequest(Long orderId, DeliveryStatus deliveryStatus, String location) {
