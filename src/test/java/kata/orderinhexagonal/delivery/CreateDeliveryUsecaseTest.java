e4a0e3e34a76cf0c8ec28f1a01776b302cb4b759
ejoongseok <ejoongseok@gmail.com>
2022-07-07 11:13:40 +0900

    배송 테스트 케이스 생성

--- src/test/java/kata/orderinhexagonal/delivery/CreateDeliveryUsecaseTest.java
+++ src/test/java/kata/orderinhexagonal/delivery/CreateDeliveryUsecaseTest.java
@@ -1,4 +1,16 @@
 package kata.orderinhexagonal.delivery;
 
+import org.junit.jupiter.api.Test;
+
 class CreateDeliveryUsecaseTest {
+
+	@Test
+	void 배송() {
+		// given
+
+		// when
+
+		// then
+	}
+
 }
