8b7c456f4e225521ed74ec694c4734166afdb786
ejoongseok <ejoongseok@gmail.com>
2022-07-07 11:16:45 +0900

    배송 테스트 케이스 Assertions 작성

--- src/test/java/kata/orderinhexagonal/delivery/CreateDeliveryUsecaseTest.java
+++ src/test/java/kata/orderinhexagonal/delivery/CreateDeliveryUsecaseTest.java
@@ -1,6 +1,9 @@
 package kata.orderinhexagonal.delivery;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import kata.orderinhexagonal.order.domain.OrderStatus;
 
 class CreateDeliveryUsecaseTest {
 
@@ -11,6 +14,12 @@ class CreateDeliveryUsecaseTest {
 		// when
 
 		// then
+		Assertions.assertThat(delivery.getId()).isPositive();
+		Assertions.assertThat(delivery.getOrder().getId()).isEqualTo(order.getId());
+		Assertions.assertThat(delivery.getOrder().getStatus()).isEqualTo(OrderStatus.DELIVERED);
+		Assertions.assertThat(delivery.getCreatedDateTime()).isNotNull();
+		Assertions.assertThat(delivery.getLocation()).isEqualTo(location);
+		Assertions.assertThat(delivery.getStatus()).isEqualTo(deliveryStatus);
 	}
 
 }
