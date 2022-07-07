5897dc516d88fee41d94d026355dd35f4b0bd98e
ejoongseok <ejoongseok@gmail.com>
2022-07-07 15:00:31 +0900

    jpql 추가

--- src/main/java/kata/orderinhexagonal/delivery/adapter/out/persistence/OrderDeliveryRepository.java
+++ src/main/java/kata/orderinhexagonal/delivery/adapter/out/persistence/OrderDeliveryRepository.java
@@ -3,7 +3,10 @@ package kata.orderinhexagonal.delivery.adapter.out.persistence;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderDeliveryRepository extends JpaRepository<OrderDeliveryEntity, Long> {
+
+	@Query("SELECT d FROM OrderDeliveryEntity d join fetch d.order join fetch d.delivery WHERE d.order.id = ?1")
 	List<OrderDeliveryEntity> findByOrderId(Long orderId);
 }
