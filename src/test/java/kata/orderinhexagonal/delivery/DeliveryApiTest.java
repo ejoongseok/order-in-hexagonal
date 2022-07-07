b990cf5e2a8ef8ee4b649293062134adba7692a6
ejoongseok <ejoongseok@gmail.com>
2022-07-07 09:35:07 +0900

    주문 배송 시작 테스트 케이스 Assertions 작성

--- src/test/java/kata/orderinhexagonal/delivery/DeliveryApiTest.java
+++ src/test/java/kata/orderinhexagonal/delivery/DeliveryApiTest.java
@@ -1,16 +1,24 @@
 package kata.orderinhexagonal.delivery;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 class DeliveryApiTest {
 
 	@Test
-	void 주문_배송() {
+	void 주문_배송_시작() {
 		// given
 
 		// when
 
 		// then
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+		Assertions.assertThat(deliveryResponse.getId()).isPositive();
+		Assertions.assertThat(deliveryResponse.getStatus()).isEqualTo(DeliveryStatus.STARTED);
+		Assertions.assertThat(deliveryResponse.getLocation()).isEqualTo(startedLocation);
+		Assertions.assertThat(deliveryResponse.getCreatedDateTime()).NotNull();
+		Assertions.assertThat(deliveryResponse.getOrderId()).isEqualTo(order.getId());
 	}
 
 }
