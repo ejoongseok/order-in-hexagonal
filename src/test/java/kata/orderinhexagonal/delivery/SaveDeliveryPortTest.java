5a17726986b0a8e6b406d0ba9fc64263963854a8
ejoongseok <ejoongseok@gmail.com>
2022-07-07 13:48:52 +0900

    배송정보 저장 테스트 케이스 Assertions 작성

--- src/test/java/kata/orderinhexagonal/delivery/SaveDeliveryPortTest.java
+++ src/test/java/kata/orderinhexagonal/delivery/SaveDeliveryPortTest.java
@@ -1,4 +1,25 @@
 package kata.orderinhexagonal.delivery;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class SaveDeliveryPortTest {
+
+	@Test
+	void 배송정보저장() {
+		// given
+
+		// when
+
+		// then
+		Assertions.assertThat(delivery.getId()).isPositive();
+		Assertions.assertThat(delivery.getStatus()).isEqualTo(deliveryStatus);
+		Assertions.assertThat(delivery.getLocation).isEqualTo(location);
+		Assertions.assertThat(deliveryEntity.getId()).isEqualTo(delivery.getId());
+		Assertions.assertThat(deliveryEntity.getStatus()).isEqualTo(delivery.getStatus());
+		Assertions.assertThat(deliveryEntity.getLocation()).isEqualTo(delivery.getLocation());
+		Assertions.assertThat(orderDelivery.getOrder.getId()).isEqualTo(order.getId());
+		Assertions.assertThat(orderDelivery.getOrder.getDelivery.getId()).isEqualTo(delivery.getId());
+	}
+
 }
