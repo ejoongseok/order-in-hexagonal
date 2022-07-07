cab3a0f30a14668ce72c26df8d9d73f06deb6ba2
ejoongseok <ejoongseok@gmail.com>
2022-07-07 13:25:31 +0900

    배송에 사용할 주문 불러오기 테스트 생성

--- src/test/java/kata/orderinhexagonal/delivery/OrderDeliveryLoadPortTest.java
+++ src/test/java/kata/orderinhexagonal/delivery/OrderDeliveryLoadPortTest.java
@@ -1,4 +1,16 @@
 package kata.orderinhexagonal.delivery;
 
+import org.junit.jupiter.api.Test;
+
 class OrderDeliveryLoadPortTest {
+
+	@Test
+	void 주문_불러오기() {
+		// given
+
+		// when
+
+		// then
+	}
+
 }
