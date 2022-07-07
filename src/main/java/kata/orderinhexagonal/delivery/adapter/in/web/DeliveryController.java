deb97441bbafab128f0cbdd305bf1fbb4757978d
ejoongseok <ejoongseok@gmail.com>
2022-07-07 11:07:58 +0900

    배송 요청 API 생성

--- src/main/java/kata/orderinhexagonal/delivery/adapter/in/web/DeliveryController.java
+++ src/main/java/kata/orderinhexagonal/delivery/adapter/in/web/DeliveryController.java
@@ -1,10 +1,17 @@
 package kata.orderinhexagonal.delivery.adapter.in.web;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import kata.orderinhexagonal.delivery.application.port.in.DeliveryRequest;
+import kata.orderinhexagonal.delivery.application.port.in.DeliveryResponse;
 
 @RestController
 @RequestMapping("/delivery")
@@ -12,7 +19,11 @@ public class DeliveryController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.OK)
-	public void delivery() {
+	public DeliveryResponse delivery(@RequestBody @Valid DeliveryRequest request, Errors errors) {
+		if (errors.hasErrors()) {
+			throw new IllegalArgumentException(errors.getAllErrors().toString());
+		}
 
+		return new DeliveryResponse();
 	}
 }
